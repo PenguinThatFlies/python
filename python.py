4f00f70f103c17deed8b6993bb611e4da8fdb28d
I.S <127890770+PenguinThatFlies@users.noreply.github.com>
2024-02-22 13:47:54 +0400

    Add files via upload

---
+++ python.py
@@ -0,0 +1,24 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+def speak_text():
+    while True:
+        text = input("Enter text to speak (or 'q' to quit): ")
+
+        if text.lower() == 'q':
+            break
+
+        rate = engine.getProperty('rate')
+        engine.setProperty('rate', rate - 50)
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[3].id)
+        try:
+            engine.say(text)
+            engine.runAndWait()
+        except RuntimeError as e:
+            print(f"Error while speaking: {e}")
+
+speak_text()
+
+engine.stop()
