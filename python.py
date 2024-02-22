701da235731c9401565f5a376e231eecb266a45c
I.S <127890770+PenguinThatFlies@users.noreply.github.com>
2024-02-22 14:11:30 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,24 +1,24 @@
 import pyttsx3
+import docx
 
 engine = pyttsx3.init()
 
-def speak_text():
-    while True:
-        text = input("Enter text to speak (or 'q' to quit): ")
+def speak_text_from_file(file_path):
+    doc = docx.Document(file_path)
+    for paragraph in doc.paragraphs:
+        text = paragraph.text
+        if text.strip():
+            rate = engine.getProperty('rate')
+            engine.setProperty('rate', rate)
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[3].id)
+            try:
+                engine.say(text)
+                engine.runAndWait()
+            except RuntimeError as e:
+                print(f"Error while speaking: {e}")
 
-        if text.lower() == 'q':
-            break
-
-        rate = engine.getProperty('rate')
-        engine.setProperty('rate', rate)
-        voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[3].id)
-        try:
-            engine.say(text)
-            engine.runAndWait()
-        except RuntimeError as e:
-            print(f"Error while speaking: {e}")
-
-speak_text()
+file_path = input("Enter the path to the Word file: ")
+speak_text_from_file(file_path)
 
 engine.stop()
